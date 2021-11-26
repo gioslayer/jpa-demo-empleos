a41eb04cc3a6d1a81bbd678435a3b0e075467a9a
gioslayer <giokreator@gmail.com>
2021-11-26 02:21:11 -0600

    Inyeccion de dependencia de CategoriasRepository sobre clase principal, para poder hacer uso del repositorio

--- src/main/java/dev/gio/JpaDemoApplication.java
+++ src/main/java/dev/gio/JpaDemoApplication.java
@@ -1,20 +1,26 @@
 package dev.gio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import dev.gio.repository.CategoriasRepository;
+
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner {
 
+	@Autowired
+	private CategoriasRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JpaDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Ejemplos de Spring JPA");
-		guardar();
+		System.out.println("iniciando el repo");
+		System.out.println(repo);
 	}
 
 	public void guardar() {
