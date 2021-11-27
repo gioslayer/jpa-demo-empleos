b2c7fdb8138130df3d1460f082a7b1792e29c9e3
gioslayer <giokreator@gmail.com>
2021-11-27 03:17:23 -0600

    Clases 81 a 84. Operaciones CRUD con JPA.

--- src/main/java/dev/gio/JpaDemoApplication.java
+++ src/main/java/dev/gio/JpaDemoApplication.java
@@ -1,10 +1,13 @@
 package dev.gio;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import dev.gio.model.Categoria;
 import dev.gio.repository.CategoriasRepository;
 
 @SpringBootApplication
@@ -19,15 +22,50 @@ public class JpaDemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("iniciando el repo");
+		System.out.println("Iniciando el repo");
 		System.out.println(repo);
+		System.out.println("Repo iniciado");
+		//guardar();
+		//buscarPorId();
+		//modificar();
+		eliminar();
 	}
 
-	public void guardar() {
-		System.out.println("Insertar un registro");
+	private void guardar() {
+		System.out.println("Insertando un registro de Categoría");
+		Categoria cat = new Categoria();
+		cat.setNombre("Finanzas");
+		cat.setDescripcion("Trabajos relacionados con finanzas y contabilidad");
+		repo.save(cat);
+		System.out.println(cat);
 	}
 	
-	public void eliminar() {
+	private void buscarPorId() {
+		System.out.println("Busqueda por id");
+		Optional<Categoria> optional = repo.findById(5);
+		if(optional.isPresent())
+			System.out.println(optional.get());
+		else
+			System.out.println("Registro no encontrado");
+	}
+	
+	private void eliminar() {
 		System.out.println("Elimina un registro");
+		int idCategoria = 5;
+		repo.deleteById(idCategoria);
+	}
+	
+	private void modificar() {
+		System.out.println("Datos actualizados de un registro");
+		Optional<Categoria> optional = repo.findById(5);
+		if(optional.isPresent()) {
+			Categoria catTmp = optional.get();
+			catTmp.setNombre("Ing. de software");
+			catTmp.setDescripcion("Desarrollo de Sistemas Computacionales");
+			repo.save(catTmp);
+			System.out.println(optional.get());
+		} else {
+			System.out.println("Categoria no encontrada");
+		}
 	}
 }
